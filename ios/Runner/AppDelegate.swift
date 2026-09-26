2312bb2a8dc6cf7e5076fdecc5062607443f8256
chubbyleonard <me@example.com>
2026-09-26 01:46:45 +0100

    update codemagic.yaml and ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,23 @@
-import Flutter
 import UIKit
+import Flutter
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // 1. Call the dummy method to force Xcode to bundle the Rust C-bindings
+    dummy_method_to_enforce_bundling()
+    
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  // 2. The dummy method itself
+  func dummy_method_to_enforce_bundling() {
+    // This is never actually executed, but it stops Xcode from stripping the FFI symbol
+    store_dart_post_cobject(nil)
   }
 }
--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,4 @@
 #import "GeneratedPluginRegistrant.h"
+
+// Add this line to expose the native Rust bridge to Swift
+extern void store_dart_post_cobject(void*);
